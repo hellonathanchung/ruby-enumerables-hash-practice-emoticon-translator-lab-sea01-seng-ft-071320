88e7734ee7ec97601a4e827925577dafee133e33
Nathan Chung <chung.nathanh@gmail.com>
2020-06-25 03:08:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,20 @@
-# require modules here
+require 'pry'
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+file = File.read(file_path)
+  emoticons = YAML.load(file)
+  end
+ # binding.pry
+  new_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  emoticon = load_libray(file_path)
+  emoji = emoticon.values.find {|faces| faces[0] == emoticon}
+  emoji == nil ? "Sorry, that emoticon was not found" : emoji[1]
 end
 
 def get_english_meaning
-  # code goes here
+
 end
